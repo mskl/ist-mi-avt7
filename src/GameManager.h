273e5618e1e64a77c2eedd4d37cbfd3652f4d8d6
Mika412 <mykhamarf17@gmail.com>
2019-10-17 20:26:43 +0100

    Boundary collision limit

--- src/GameManager.h
+++ src/GameManager.h
@@ -51,6 +51,14 @@ public:
     float lightPos[4] = {4.0f, 6.0f, 2.0f, 1.0f};
     float playerPos[3] = {0.0f, 1.0f, 0.0f};
 
+    bool isPlaying = true;
+
+    float topVerticalLimitPlayerPos = -6.0f;
+    float bottomVerticalLimitPlayerPos = 6.0f;
+
+    float leftHorizontalLimitPlayerPos = -6.0f;
+    float rightHorizontalLimitPlayerPos = 6.0f;
+
     enum CameraType {
         CAMERA_PERSPECTIVE_FOLLOW,
         CAMERA_PERSPECTIVE_FIXED,
@@ -86,6 +94,8 @@ public:
     void processKeys(unsigned char key, int xx, int yy)
     {
         printf("Key value (%d)\n", key);
+
+        // TODO: This should be extracted to the player update function
         switch(key) {
             // Escape exits the game
             case 27: glutLeaveMainLoop(); break;
@@ -95,10 +105,13 @@ public:
             case 'n': glDisable(GL_MULTISAMPLE); break;
 
             // Player movement
-            case 'p': playerPos[0] += 1; break;
-            case 'o': playerPos[0] -= 1; break;
-            case 'q': playerPos[2] -= 1; break;
-            case 'a': playerPos[2] += 1; break;
+            case 'p': movePlayerHorizontally(1); break;
+            case 'o': movePlayerHorizontally(-1); break;
+            case 'q': movePlayerVertically(-1); break;
+            case 'a': movePlayerVertically(1); break;
+
+            // Stop/Continue game
+            case 's': isPlaying = !isPlaying; break;
 
             // CameraType
             case '1':
@@ -108,6 +121,30 @@ public:
             case '3':
                 selectedCamera = CAMERA_ORTHO; break;
         }
+    }
+
+    void movePlayerVertically(float dir){
+        if(!isPlaying)
+            return;
+
+        float tempPos = playerPos[2] + dir;
+
+        if(tempPos < topVerticalLimitPlayerPos || tempPos > bottomVerticalLimitPlayerPos)
+            return;
+
+        playerPos[2] = tempPos;
+    }
+
+    void movePlayerHorizontally(float dir){
+        if(!isPlaying)
+            return;
+
+        float tempPos = playerPos[0] + dir;
+
+        if(tempPos > rightHorizontalLimitPlayerPos || tempPos < leftHorizontalLimitPlayerPos)
+            return;
+
+        playerPos[0] = tempPos;
     }
 
     GLuint setupShaders()
