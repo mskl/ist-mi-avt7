e8a4f90eab880983e544dee16d5f194dfe6e8dd5
mykha <mykhamarf17@gmail.com>
2019-10-17 19:12:04 +0100

    Added BoundingBox Class

--- src/BoundingBox.h
+++ src/BoundingBox.h
@@ -5,4 +5,18 @@
 #ifndef AVT7_BOUNDINGBOX_H
 #define AVT7_BOUNDINGBOX_H
 
+#include "Vector3.h"
+
+class BoundingBox {
+protected:
+    Vector3 vecMin;
+    Vector3 vecMax;
+public:
+    BoundingBox();
+    ~BoundingBox();
+    void setMin(Vector3 pos);
+    void setMax(Vector3 pos);
+    bool isColliding(BoundingBox b);
+    bool isInside(BoundingBox b);
+};
 #endif //AVT7_BOUNDINGBOX_H
